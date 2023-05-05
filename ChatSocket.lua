93297e824fdd26c14c3b5816132c4bf58033a4f0
Mana42138 <86923626+Mana42138@users.noreply.github.com>
2023-05-05 16:30:01 +0200

    Create ChatSocket.lua

---
+++ ChatSocket.lua
@@ -0,0 +1,32 @@
+local decode = require(script.decode)
+
+local ws = function(dict)
+	local API = dict.url or 'you can also paste you link here if you dont want to show it in the Socket table!'
+	local server = game:GetService("HttpService")
+	local onMessage = dict.onMessage or function(msg)
+		decode.decode(msg, function(t)
+			print(t)
+		end)
+	end
+	local function sendMessage(msg)
+		server:GetAsync(API..'send/?msg='..tostring(msg)..'&server=false')
+	end
+	
+	local loop = coroutine.create(function()
+		while wait(1) do
+			local msg = server:GetAsync(API..'poll/')
+			if msg ~= '[]\n' then
+				onMessage(msg)
+			end
+		end
+	end)
+	coroutine.resume(loop)
+	
+	return {
+		Url = API,
+		onMessage = onMessage,
+		sendMessage = sendMessage
+	}
+end
+
+return ws
