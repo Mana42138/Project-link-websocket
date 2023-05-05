7c3126ab68513cc002ad44900581d993e3b93e1f
Mana42138 <86923626+Mana42138@users.noreply.github.com>
2023-05-05 16:38:19 +0200

    Delete SendMessage-python.py

--- SendMessage-python.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-# &server=false = not server message
-# &server=true = servr message
-message = 'noobs?'
-response = requests.get('https://chatting.madsbrriinckbas.repl.co/api/send/?msg='+message+'&server=false')
