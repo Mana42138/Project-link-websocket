8fa8c7b83eec128d5c1e7c474d7dd73bd0909aa2
Mana42138 <86923626+Mana42138@users.noreply.github.com>
2023-05-06 22:08:24 +0200

    Update Decoder.lua

--- decode/Decoder.lua
+++ decode/Decoder.lua
@@ -1,10 +1,14 @@
-local decode = function(msg, callback)
+local module = {}
+module.decode = function(msg, callback)
 	local messages = game:GetService("HttpService"):JSONDecode(msg)
-	for i, message in ipairs(messages) do
+	for i, message in pairs(messages) do
 		pcall(callback, message)
 	end
 end
-
-return {
-	decode = decode
-}
+module.decodeScript = function(data, callback)
+	local messages = game:GetService("HttpService"):JSONDecode(data)
+	for i, message in pairs(messages) do
+		pcall(callback, message)
+	end
+end
+return module
