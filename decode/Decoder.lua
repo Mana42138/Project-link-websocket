4497dc2b318a9d8f312a83a325d1639a2bbdd783
Mana42138 <86923626+Mana42138@users.noreply.github.com>
2023-05-11 13:59:20 +0200

    Update Decoder.lua
    Updated the decoder and i know some functions are the same but i do not care

--- decode/Decoder.lua
+++ decode/Decoder.lua
@@ -1,14 +1,44 @@
 local module = {}
+local http = game:GetService("HttpService")
+local message_table = {}
+
 module.decode = function(msg, callback)
-	local messages = game:GetService("HttpService"):JSONDecode(msg)
-	for i, message in pairs(messages) do
-		pcall(callback, message)
+	for i,v in pairs(msg) do
+		for i,v in pairs(v) do
+				table.insert(message_table, v)
+				pcall(callback, message_table)
+				return message_table
+		end
 	end
+	table.clear(message_table)
 end
+
 module.decodeScript = function(data, callback)
-	local messages = game:GetService("HttpService"):JSONDecode(data)
-	for i, message in pairs(messages) do
+	for i,v in pairs(data) do
+		for i,v in pairs(v) do
+			pcall(callback, v)
+		end
+	end
+end
+
+module.decodeChat = function(data, callback)
+	for i, message in pairs(data) do
 		pcall(callback, message)
 	end
 end
+
+function module.isScript(data)
+	for i,v in pairs(data) do
+		for i,v in pairs(v) do
+			return v.isScript
+		end
+	end
+end
+
+function module.getscriptdata(data, callback)
+	for i,v in pairs(data) do
+		pcall(callback, v)
+	end
+end
+
 return module
